10118af8dfca54d2f10eebdeebfc01202007fd7f
Branislav Ševc <xsevc@fi.muni.cz>
2021-03-30 13:21:10 +0200

    improve eval output

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,8 +2,32 @@
 
 out_dir="output"
 
+scores=""
+lines=""
+
 for ref_file in pa193_dataset/dataset/*.json; do
     out_file="$out_dir/$(basename "$ref_file")"
     echo "evaluating \"$out_file\"..."
-    python3 pa193_dataset/output_compare.py "$out_file" "$ref_file"
+    score=$(python3 pa193_dataset/output_compare.py "$out_file" "$ref_file")
+    scores="$scores $score"
+    lines="$lines\n$score $(basename "$out_file")"
 done
+
+# results
+echo ""
+echo -e "$lines" | sort -nr
+
+# stats
+sum=0
+count=0
+min=100
+max=0
+
+for score in $scores; do
+    ((sum+=score))
+    ((count++))
+    min=$((score < min ? score : min))
+    max=$((score > max ? score : max))
+done
+
+echo "total: $sum/$((count * 100)), min: $min, max: $max"
