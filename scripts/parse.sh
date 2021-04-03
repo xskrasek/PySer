d5c9a43b848a3fcda63fdd62b047a0f6a767dd4e
Štěpán Horáček <shoracek@redhat.com>
2021-04-03 12:56:29 +0200

    Support multiple files

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -6,12 +6,5 @@ out_dir="output"
 
 mkdir -p "$out_dir"
 
-i=0
-batch_size=10
+python3 src/main.py --output_folder="$out_dir" pa193_dataset/dataset/*.txt
 
-for in_file in pa193_dataset/dataset/*.txt; do
-    out_file="$out_dir/$(basename "$in_file" .txt).json"
-    echo "parsing \"$out_file\"..."
-    python3 src/main.py "$in_file" --output_file "$out_file" &
-    ((i = (i + 1) % batch_size)); ((i == 0)) && wait
-done
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import parser
+import os
+from typing import List
 
 import argparse
 
@@ -15,13 +17,24 @@ def generate_json_file(input_path: str, output_path: str):
         file.write(output)
 
 
+def generate_multiple_json_files(input_files: List[str], output_folder: str):
+    for input_file in input_files:
+        basename = os.path.splitext(input_file)[0]
+        basename = os.path.basename(basename)
+        output_file = output_folder + "/" + basename + ".json"
+
+        generate_json_file(input_file, output_file)
+
+
 def parse_args():
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument("input_file", type=str)
-    argument_parser.add_argument("--output_file", type=str, default="output.json")
+    argument_parser.add_argument("input_files", type=str, nargs='+')
+    # TODO: might be a good idea to remove some of those,
+    # looks like a possible security hole?
+    argument_parser.add_argument("--output_folder", type=str, default=".")
     return argument_parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    generate_json_file(args.input_file, args.output_file)
+    generate_multiple_json_files(args.input_files, args.output_folder)
