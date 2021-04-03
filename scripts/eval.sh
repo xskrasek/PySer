4768c675e5df5091dd902322792087bdea100d0a
Branislav Ševc <xsevc@fi.muni.cz>
2021-04-03 15:30:24 +0200

    fix exit code handling in eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -6,15 +6,20 @@ out_dir="output"
 
 scores=""
 lines=""
+exit_code=0
 
 for ref_file in pa193_dataset/dataset/*.json; do
     out_file="$out_dir/$(basename "$ref_file")"
     echo "evaluating \"$out_file\"..."
-    out=$(python3 pa193_dataset/output_compare.py "$out_file" "$ref_file" -v)
-    score=$(echo "$out" | head -n 1)
-    partial_scores=$(echo "$out" | tail -n 1)
-    scores="$scores $(echo "$score" | head -n 1)"
-    lines="$lines\n $score $partial_scores $(basename "$out_file")"
+    if out=$(python3 pa193_dataset/output_compare.py "$out_file" "$ref_file" -v)
+    then
+        score=$(echo "$out" | head -n 1)
+        partial_scores=$(echo "$out" | tail -n 1)
+        scores="$scores $(echo "$score" | head -n 1)"
+        lines="$lines\n $score $partial_scores $(basename "$out_file")"
+    else
+        exit_code=1;
+    fi
 done
 
 # results
@@ -33,3 +38,5 @@ done
 
 possible=$((count * 100))
 echo "total: $sum/$possible"
+
+exit $exit_code
