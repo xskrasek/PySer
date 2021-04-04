6f3758cc6a2c269458c39ae328a4ca5d12cc252e
Branislav Ševc <xsevc@fi.muni.cz>
2021-04-04 13:00:15 +0200

    moved evaluation stats into output_compare.py

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -4,32 +4,4 @@ trap exit SIGINT
 
 out_dir="output"
 
-scores=""
-lines=""
-
-for ref_file in pa193_dataset/dataset/*.json; do
-    out_file="$out_dir/$(basename "$ref_file")"
-    echo "evaluating \"$out_file\"..."
-    out=$(python3 pa193_dataset/output_compare.py "$out_file" "$ref_file" -v)
-    score=$(echo "$out" | head -n 1)
-    partial_scores=$(echo "$out" | tail -n 1)
-    scores="$scores $(echo "$score" | head -n 1)"
-    lines="$lines\n $score $partial_scores $(basename "$out_file")"
-done
-
-# results
-echo ""
-echo "sum tit ver toc rev bib name"
-echo -e "$lines" | sort -nr
-
-# total
-sum=0
-count=0
-
-for score in $scores; do
-    ((sum+=score))
-    ((count++))
-done
-
-possible=$((count * 100))
-echo "total: $sum/$possible"
+python3 pa193_dataset/output_compare.py "$out_dir" pa193_dataset/dataset/ -v
