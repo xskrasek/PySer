e44e5c1e26066b3a1756e34194fa02a0b9fdb201
Štěpán Horáček <shoracek@redhat.com>
2021-04-14 16:33:08 +0200

    Find revisions

--- src/parser.py
+++ src/parser.py
@@ -137,13 +137,46 @@ def find_table_of_contents(input_original: str) -> List[List[str]]:
     return result
 
 
+def find_revisions(input: str) -> List[Dict[str, str]]:
+    # find start
+    iter = re.finditer(r"revision history|version control", input, re.IGNORECASE)
+    start = None
+    a = 0
+    for start in iter:
+        a += 1
+        # first mention probably in toc
+        if a == 2:
+            break
+    if not start:
+        return []
+    input = input[start.span(0)[1]:]
+
+    # find end
+    end = re.search(r"\n\n\n\n", input, re.IGNORECASE)
+    if end:
+        input = input[:end.span(0)[1]]
+    else:
+        input = input[:5000]
+    
+    result = re.findall(r"\s+([0-9.]+)\s+([0-9-]+)?\s+(.*)", input)
+    
+    for i in range(len(result)):
+        result[i] = {
+            "version": result[i][0],
+            "date": result[i][1],
+            # there can be more information, like author...
+            "description": result[i][2].split("  ")[-1],
+        }
+
+    return result
+
 def generate_json(input: str) -> str:
     return json.dumps(
         {
             "title": find_title(input),
             "versions": find_versions(input),
             "table_of_contents": find_table_of_contents(input),
-            "revisions": [],
+            "revisions": find_revisions(input),
             "bibliography": find_bibliography(input),
             "other": [],
         }, indent=4, ensure_ascii=False)
