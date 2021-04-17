b1f3d4c345c2b9aaa3cee7056023cfa34efee066
Štěpán Horáček <shoracek@redhat.com>
2021-04-17 17:05:04 +0200

    Improve revision parsing
    Clean revision date
    
    Support more revision formats
    
    Parse different revision formats
    
    Ignore in text mentions of revision history
    
    Accept also colons as delimiters in revisions
    
    Support revisions without date even more

--- src/revisions_parser.py
+++ src/revisions_parser.py
@@ -2,37 +2,98 @@ import re
 from typing import Dict, List
 
 
-def parse(input: str) -> List[Dict[str, str]]:
-    # find start
-    iter = re.finditer(r"revision history|version control", input,
-                       re.IGNORECASE)
-    start = None
-    a = 0
-    for start in iter:
-        a += 1
-        # first mention probably in toc
-        if a == 2:
-            break
-    if not start:
-        return []
-    input = input[start.span(0)[1]:]
+def month_to_number(input: str) -> str:
+    values = {
+        "jan": "01",
+        "feb": "02",
+        "mar": "03",
+        "apr": "04",
+        "may": "05",
+        "jun": "06",
+        "jul": "07",
+        "aug": "08",
+        "sep": "09",
+        "oct": "10",
+        "nov": "11",
+        "dec": "12",
+    }
+    form = input[:3].lower()
+    if form in values:
+        return values[form]
+    return input
 
-    # find end
+
+def clean_date(input: str) -> str:
+    if not input:
+        return ""
+    
+    splitted = re.split("\.|-", input)
+    if len(splitted) != 3:
+        return input
+
+    # swap year and day
+    if int(splitted[0]) < 1000:
+        splitted.reverse()
+
+    # use number for months
+    splitted[1] = month_to_number(splitted[1])
+
+    return "-".join(splitted)
+
+
+def parse_general(input: str, regex: str, ver_index: int, date_index: int) -> List[Dict[str, str]]:
     end = re.search(r"\n{4}", input, re.IGNORECASE)
-    if end:
-        input = input[:end.span(0)[1]]
-    else:
-        input = input[:5000]
-    
-    result = re.findall(r"\s+([0-9.]+)\s+([0-9-]+)?\s+(.*)", input)
-    
-    for i in range(len(result)):
-        result[i] = {
-            "version": result[i][0],
-            "date": result[i][1],
-            # there can be more information, like author...
-            "description": result[i][2].split("  ")[-1],
-        }
+    input = input[:end.span(0)[1]] if end else input[:5000]
 
-    return result
+    results = re.findall(regex, input)
 
+    final_results = []
+    for result in results:
+        version = result[ver_index]
+        date = clean_date(result[date_index])
+        # there can be more information, like author...
+        description = result[2].split("  ")[-1]
+
+        final_results.append({
+            "version": version,
+            "date": date,
+            "description": description,
+        })
+    return final_results
+
+
+def parse_rev_date_desc(input: str) -> List[Dict[str, str]]:
+    return parse_general(input, r"\s+v?([0-9.]+)\s+([0-9-A-Za-z-\.]+)?[\s:]\s+(.*)", 0, 1)
+
+
+def parse_date_ver_desc(input: str) -> List[Dict[str, str]]:
+    return parse_general(input, r"\s+v?([0-9-A-Za-z-\.]+)?\s+([0-9.]+)[\s:]\s+(.*)", 1, 0)
+
+
+def parse(input: str) -> List[Dict[str, str]]:
+    found = re.search(r"^\w*rev\w*\s+date\s+.*description",
+                      input, re.IGNORECASE | re.MULTILINE)
+    if found:
+        return parse_rev_date_desc(input[found.span(0)[1]:])
+
+    found = re.search(r"date\s+ver\w*\s+.*description", input, re.IGNORECASE)
+    if found:
+        return parse_date_ver_desc(input[found.span(0)[1]:])
+
+    found = re.search(r"version\s\s+description", input, re.IGNORECASE)
+    if found:
+        return parse_rev_date_desc(input[found.span(0)[1]:])
+
+    # final attempt, filtering out capitalization that could be used in normal text
+    iter = re.finditer(r"REVISION HISTORY|Revision [Hh]istory|VERSION CONTROL|Version [Cc]ontrol", input)
+    iter_list = list(iter)
+    if iter_list:
+        # first mention probably in toc, so take the second one, if there is one
+        if len(iter_list) < 2:
+            start = iter_list[0]
+        else:
+            start = iter_list[1]
+
+        return parse_rev_date_desc(input[start.span(0)[1]:])
+
+    return []
