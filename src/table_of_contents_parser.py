1dd4cc48f0f05a881b023da9a571cab66e520418
Branislav Ševc <xsevc@fi.muni.cz>
2021-04-18 14:34:38 +0200

    TOC - refactor

--- src/table_of_contents_parser.py
+++ src/table_of_contents_parser.py
@@ -5,7 +5,26 @@ from operator import itemgetter
 from typing import List, Tuple, Optional
 
 
-def find_dotted_toc(input: str) -> Optional[str]:
+RE_TOC_WITH_DOTS = re.compile(
+    # manual negative lookbehind
+    r"((?:Table|Figure|(?:Fig|Tab)\.?) |[^\s1-9])?"
+    r"([A-D1-9][0-9]?(?:\.[0-9]{1,2})*)\.?" # section number
+    r" {1,20}"
+    r"([A-Z](?:[^\.](?! {6,})|\.(?! ?\.))+)" # title
+    r" ?(?:(?:\.){2,}|(?:\.\s){2,}) ?" # dots
+    r"([0-9]+)") # page number
+
+RE_TOC_WITHOUT_DOTS = re.compile(
+    # manual negative lookbehind
+    r"((?:Table|Figure|(?:Fig|Tab)\.?) |[^\s1-9])?"
+    r"([A-D1-9][0-9]?(?:\.[0-9]{1,2})*)\.?" # section number
+    r" {1,20}"
+    r"(.*)" #r"(.*[^\s].*)" # title
+    r" {5,}"
+    r"([0-9]+)") # page number
+
+
+def find_toc_with_dots(input: str) -> Optional[str]:
     def is_dotted_line(line: str) -> bool:
         return len(line) >= 20 and line.count('.') >= len(line) * 0.1
 
@@ -27,7 +46,52 @@ def find_dotted_toc(input: str) -> Optional[str]:
     return "\n".join(lines[start:end])
 
 
-def postprocess_match(match: Tuple[str, str, str, str]) -> Optional[Tuple[str, str, int]]:
+def parse_toc_with_dots_multiple_columns(input: str):
+    lines = input.split("\n")
+    
+    matches = []
+    left_column = ""
+    right_column = ""
+
+    for line in lines:
+        match_iter = RE_TOC_WITH_DOTS.finditer(line)
+
+        try:
+            match = next(match_iter)
+            lookbehind, id, title, page = match.groups()
+            matches.append((lookbehind, id, title, page))
+            left_column += squash_whitespace(line[:match.start()]) + '\n'
+            right_column += squash_whitespace(line[match.end():]) + '\n'
+        except StopIteration:
+            # NOTE: This is the weak point. We have tried counting in also
+            # the surrounding page and section numbers.
+            spaces = list(re.finditer(r"\s{2,}", line))
+            if len(spaces) != 0:
+                largest_space = max(
+                    spaces, key=lambda match: match.end() - match.start())
+                left = line[:largest_space.start()]
+                right = line[largest_space.end():]
+                left_column += squash_whitespace(left) + '\n'
+                right_column += squash_whitespace(right) + '\n'
+
+    result = postprocess_matches(matches)
+    result += parse_toc_with_dots(left_column)
+    result += parse_toc_with_dots(right_column)
+    return result
+
+
+def parse_toc_with_dots(input: str) -> List[Tuple[str, str, int]]:
+    matches = RE_TOC_WITH_DOTS.findall(input, re.MULTILINE)
+    return postprocess_matches(matches) if matches is not None else []
+
+
+def parse_toc_without_dots(input: str) -> List[Tuple[str, str, int]]:
+    matches = RE_TOC_WITHOUT_DOTS.findall(input)
+    return postprocess_matches(matches) if matches is not None else []
+
+
+def postprocess_match(match: Tuple[str, str, str, str]) \
+-> Optional[Tuple[str, str, int]]:
     if match[0] is not None and len(match[0]) != 0:
         return None
 
@@ -37,76 +101,9 @@ def postprocess_match(match: Tuple[str, str, str, str]) -> Optional[Tuple[str, s
     return id, title, page
 
 
-def postprocess_matches(matches: List[Tuple[str, str, str, str]]) -> List[Tuple[str, str, int]]:
+def postprocess_matches(matches: List[Tuple[str, str, str, str]]) \
+-> List[Tuple[str, str, int]]:
     return list([r for r in map(postprocess_match, matches) if r is not None])
-
-
-def parse_without_dots(input_original: str) -> List[Tuple[str, str, int]]:
-    # Look at the start and the end of the document
-    start = 0.15
-    end = -start
-    input = input_original[:int(len(input_original) * start)] + \
-            input_original[int(len(input_original) * end):]
-
-    matches = re.findall(r"((?:Table|Figure|(?:Fig|Tab)\.?) |[^\s1-9])?" # manual negative lookbehind
-                         r"([A-D1-9][0-9]?(?:\.[0-9]{1,2})*)\.?" # section number
-                         r" {1,20}"
-                         r"(.*)" #r"(.*[^\s].*)" # title
-                         r" {5,}"
-                         r"([0-9]+)", # page number
-                         input)
-
-    return postprocess_matches(matches) if matches is not None else []
-
-
-def parse_with_dots(input: str) -> List[Tuple[str, str, int]]:
-    matches = re.findall(r"((?:Table|Figure|(?:Fig|Tab)\.?) |[^\s1-9])?" # manual negative lookbehind
-                         r"([A-D1-9][0-9]?(?:\.[0-9]{1,2})*)\.?" # section number
-                         r" {1,20}"
-                         r"([A-Z](?:[^\.](?! {6,})|\.(?! ?\.))+)" # title
-                         r" ?(?:(?:\.){2,}|(?:\.\s){2,}) ?" # dots
-                         r"([0-9]+)", # page number
-                         input, re.MULTILINE)
-
-    return postprocess_matches(matches) if matches is not None else []
-
-
-def parse_with_dots_multiple_columns(input: str):
-    lines = input.split("\n")
-
-    regex = re.compile(r"((?:Table|Figure|(?:Fig|Tab)\.?) |[^\s1-9])?" # manual negative lookbehind
-                       r"([A-D1-9][0-9]?(?:\.[0-9]{1,2})*)\.?" # section number
-                       r" {1,20}"
-                       r"([A-Z](?:[^\.](?! {6,})|\.(?! ?\.))+)" # title
-                       r" ?(?:(?:\.){2,}|(?:\.\s){2,}) ?" # dots
-                       r"([0-9]+)") # page number
-    
-    matches = []
-    left_column = ""
-    right_column = ""
-
-    for line in lines:
-        match_iter = regex.finditer(line)
-
-        try:
-            match = next(match_iter)
-            lookbehind, id, title, page = match.groups()
-            matches.append((lookbehind, id, title, page))
-            left_column += squash_whitespace(line[:match.start()]) + '\n'
-            right_column += squash_whitespace(line[match.end():]) + '\n'
-        except StopIteration:
-            # NOTE: This is the weak point. We have tried counting in also the surrounding page
-            # and section numbers.
-            spaces = list(re.finditer(r"\s{2,}", line))
-            if len(spaces) != 0:
-                largest_space = max(spaces, key=lambda match: match.end() - match.start())
-                left_column += squash_whitespace(line[:largest_space.start()]) + '\n'
-                right_column += squash_whitespace(line[largest_space.end():]) + '\n'
-
-    result = postprocess_matches(matches)
-    result += parse_with_dots(left_column)
-    result += parse_with_dots(right_column)
-    return result
 
 
 def sort(result: List[Tuple[str, str, int]]) -> List[Tuple[str, str, int]]:
@@ -138,13 +135,17 @@ def sort(result: List[Tuple[str, str, int]]) -> List[Tuple[str, str, int]]:
 
 
 def parse(input: str) -> List[Tuple[str, str, int]]:
-    dotted_toc = find_dotted_toc(input)
+    dotted_toc = find_toc_with_dots(input)
 
     if dotted_toc is not None:
-        result = parse_with_dots(dotted_toc)
-        result = parse_with_dots_multiple_columns(dotted_toc)
+        result = parse_toc_with_dots(dotted_toc)
+        result = parse_toc_with_dots_multiple_columns(dotted_toc)
     else:
-        result = parse_without_dots(input)
+        # Look at the start and the end of the document
+        start = 0.15
+        end = -start
+        result = parse_toc_without_dots(input[:int(len(input) * start)])
+        result += parse_toc_without_dots(input[int(len(input) * end):])
 
     result = sort(result)
     return result
