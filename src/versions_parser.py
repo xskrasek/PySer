b07f6c49d55e3e0fd539f434ef3f339b1f676a71
Štěpán Horáček <shoracek@redhat.com>
2021-04-19 11:06:24 +0200

    Versions improvement
    Parse versions as complete words

--- src/versions_parser.py
+++ src/versions_parser.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 
 
 def parse_eal(input: str) -> List[str]:
-    found = re.findall(r"EAL ?[0-9]\+?", input)
+    found = re.findall(r"[^\w](EAL ?[0-9]\+?)", input)
 
     return deduplicate_list(found)
 
@@ -23,6 +23,7 @@ def parse_sha(input: str) -> List[str]:
 
 def parse_des(input: str) -> List[str]:
     found = re.findall(r"3des", input, re.IGNORECASE)
+    found += re.findall(r"des3", input, re.IGNORECASE)
     found += re.findall(r"triple-des", input, re.IGNORECASE)
     found += re.findall(r"tdes", input, re.IGNORECASE)
 
@@ -49,13 +50,29 @@ def parse_ecc(input: str) -> List[str]:
     return deduplicate_list(found)
 
 
+def parse_global_platform(input: str) -> List[str]:
+    found = re.findall(r"global platform (?:[0-9]\.)*[0-9]", input, re.IGNORECASE)
+
+    return deduplicate_list(found)
+
+
+def parse_java_card(input: str) -> List[str]:
+    found = re.findall(r"java card (?:[0-9]\.)*[0-9]", input, re.IGNORECASE)
+    
+    return deduplicate_list(found)
+
+
 def parse(input: str) -> Dict[str, List[str]]:
     versions = {}
-    for (version_name, parse_function) in [("eal", parse_eal),
-                                           ("sha", parse_sha),
-                                           ("des", parse_des),
-                                           ("rsa", parse_rsa),
-                                           ("ecc", parse_ecc)]:
+    for (version_name, parse_function) in [
+        ("eal", parse_eal),
+        ("sha", parse_sha),
+        ("des", parse_des),
+        ("rsa", parse_rsa),
+        ("ecc", parse_ecc),
+        ("global_platform", parse_global_platform),
+        ("java_card", parse_java_card)
+    ]:
         result = parse_function(input)
         if result:
             versions[version_name] = result
