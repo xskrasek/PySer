641ecf78bfb196e09629eb83614e350d914f3e48
Branislav Ševc <xsevc@fi.muni.cz>
2021-04-21 15:05:55 +0200

    fix typing

--- src/pretty_printer.py
+++ src/pretty_printer.py
@@ -1,6 +1,7 @@
 from common import parsed_fields_long, parsed_fields_short
+from functools import partial
 import re
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 
 
 def pretty_field_name(field: str) -> str:
@@ -18,11 +19,11 @@ def pretty_version_key(key: str) -> str:
         return "".join(splits)
 
 
-def pretty_print_title(data: str):
+def pretty_print_title(data: str) -> None:
     print(data)
 
 
-def pretty_print_versions(data: Dict[str, List[str]]):
+def pretty_print_versions(data: Dict[str, List[str]]) -> None:
     labels_size = max(len(pretty_version_key(key)) for key in data.keys())
     
     for key, versions in sorted(data.items()):
@@ -31,7 +32,7 @@ def pretty_print_versions(data: Dict[str, List[str]]):
               sep=" " * 4)
 
 
-def pretty_print_table_of_contents(data: List[Tuple[str, str, int]]):
+def pretty_print_table_of_contents(data: List[Tuple[str, str, int]]) -> None:
     col1_size = max(len(x[0]) for x in data)
     col2_size = max(len(x[1]) for x in data) + 4
     max_page_size = max(len(str(x[2])) for x in data)
@@ -43,7 +44,7 @@ def pretty_print_table_of_contents(data: List[Tuple[str, str, int]]):
               page)
 
 
-def pretty_print_revisions(data: List[Dict[str, str]]):
+def pretty_print_revisions(data: List[Dict[str, str]]) -> None:
     col1_title = "version"
     col2_title = "date"
     col3_title = "description"
@@ -63,14 +64,14 @@ def pretty_print_revisions(data: List[Dict[str, str]]):
               sep=" " * 4)
 
 
-def pretty_print_bibliography(data: Dict[str, str]):
+def pretty_print_bibliography(data: Dict[str, str]) -> None:
     if all(key[1:-1].isnumeric() for key in data.keys()):
-        def sorting_key(key_value: Tuple[str, str]) -> List[int]:
+        def sorting_key(key_value: Tuple[str, str]) -> Union[str, List[int]]:
             splits = re.split(r"_|-", key_value[0][1:-1])
             num_splits = [int(split) for split in splits]
             return num_splits
     else:
-        def sorting_key(key_value: Tuple[str, str]) -> str:
+        def sorting_key(key_value: Tuple[str, str]) -> Union[str, List[int]]:
             return key_value[0]
 
     labels_size = max(len(key) for key in data.keys())
@@ -79,21 +80,21 @@ def pretty_print_bibliography(data: Dict[str, str]):
         print(key.ljust(labels_size), text, sep=" " * 4)
 
 
-def pretty_print(data, fields: List[str]):
-    functions = {
-        "title": pretty_print_title,
-        "versions": pretty_print_versions,
-        "table_of_contents": pretty_print_table_of_contents,
-        "revisions": pretty_print_revisions,
-        "bibliography": pretty_print_bibliography
+def pretty_print(data, fields: List[str]) -> None:
+    pretty_printing_functions = {
+        "title": partial(pretty_print_title, data["title"]),
+        "versions": partial(pretty_print_versions, data["versions"]),
+        "table_of_contents": partial(pretty_print_table_of_contents, data["table_of_contents"]),
+        "revisions": partial(pretty_print_revisions, data["revisions"]),
+        "bibliography": partial(pretty_print_bibliography, data["bibliography"])
     }
 
     for i, field in enumerate(fields):
-        function = functions[field]
+        pretty_printing_function = pretty_printing_functions[field]
         
         if i != 0:
             print()
         print(pretty_field_name(field) + ":")
     
-        function(data[field])
+        pretty_printing_function()
 
