e0dc64c5e66c7e316ec0a5e53c66a430d98775ee
Štěpán Horáček <stepan.horacek@gmail.com>
2021-04-25 13:36:36 +0200

    Fix squash_whitespace

--- src/common.py
+++ src/common.py
@@ -8,7 +8,9 @@ parsed_fields_short = ["tit", "ver", "toc", "rev", "bib"]
 
 
 def squash_whitespace(plain_text: str) -> str:
-    return " ".join(plain_text.split()).replace("\n", " ")
+    replaced_dash_newline = plain_text.replace("-\n", "-")
+    replaced_all_newlines = " ".join(replaced_dash_newline.split())
+    return replaced_all_newlines
 
 
 def deduplicate_list(plain_text: List) -> List:
